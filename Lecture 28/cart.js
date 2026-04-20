c3e4b506505d31f8a9b97369a639a5b3bf4ed1de
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-04-20 17:09:37 +0530

    ...

--- Lecture 28/cart.js
+++ Lecture 28/cart.js
@@ -1,10 +1,20 @@
 const cartDiv = document.getElementById("cart-div")
+const totalPrice = document.getElementById("total-price")
 
+
+function removeCart(i) {
+    let arr = JSON.parse(localStorage.getItem("carts")) || [];
+    arr.splice(i, 1);
+    localStorage.setItem("carts", JSON.stringify(arr));
+    displayCartProducts();
+}
 
 function displayCartProducts() {
+    let total = 0;
     const carts = JSON.parse(localStorage.getItem("carts")) || [];
     cartDiv.innerHTML = "";
-    carts.forEach(element => {
+    carts.forEach((element, i) => {
+        total = total + (element.price * 93);
         const div = document.createElement("div")
         div.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
@@ -14,14 +24,16 @@ function displayCartProducts() {
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">${element.title}</h5>
-                            <p class="card-text">${element.price}</p>
-                            <p class="card-text"><small class="text-body-secondary">${element.category}</small></p>
+                            <p class="card-text">₹${element.price * 93}/-</p>
+                            <p class="card-text"><small class="text-body-secondary">Qty ${element.qty}</small></p>
                         </div>
+                        <button onclick="removeCart(${i})" class="btn btn-danger">Remove</button>
                     </div>
                 </div>
             </div>`;
         cartDiv.appendChild(div)
     });
+    totalPrice.textContent = "Total : ₹ " + (total.toFixed(2)) + "/-";
 }
 
 displayCartProducts();
--- Lecture 28/script.js
+++ Lecture 28/script.js
@@ -1,10 +1,18 @@
 const productsDivison = document.getElementById("products-div")
+let allProducts = [];
+
 
 async function addToCart(i) {
-    const res = await fetch("https://dummyjson.com/products")
-    const data = await res.json();
     const arr = JSON.parse(localStorage.getItem("carts")) || [];
-    arr.push(data.products[i]);
+    const res = arr.findIndex((e) => e.id == allProducts[i].id)
+
+    if (res == -1) { 
+        allProducts[i].qty = 1;
+        arr.push(allProducts[i]);
+    } else {
+        arr[res].qty++;
+    }
+
     localStorage.setItem("carts", JSON.stringify(arr));
 }
 
@@ -19,6 +27,7 @@ function displayProducts(products) {
                 <div class="card-body">
                     <h5 class="card-title">${product.title}</h5>
                     <p class="card-text">${product.category}</p>
+                    <p class="card-text">Qty ${product.qty}</p>
                     <p class="card-text fs-5">Price ₹${(product.price * 93).toFixed(2)}/-</p>
                     <button onclick="addToCart(${i})" class="btn btn-primary">Add to Cart</button>
                 </div>
@@ -32,13 +41,13 @@ function displayProducts(products) {
 async function fetchProducts() {
     const res = await fetch("https://dummyjson.com/products");
     const data = await res.json();
+    allProducts = data.products;
     displayProducts(data.products)
 }
 
 
 
 fetchProducts();
-
 
 // local storage - 10mb - text - string
 
@@ -48,3 +57,5 @@ fetchProducts();
 // const a = [1,2,3]
 // a.push(4)
 // a = [1,2,3,4]
+
+// 3 hrs 
