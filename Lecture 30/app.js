7f2f8702fa70dd86dff2efa10dc6fcb75a28817a
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-05-04 16:56:02 +0530

    ...

---
+++ Lecture 30/app.js
@@ -0,0 +1,21 @@
+export const a = 10;
+
+export const arr = [1, 2, 3, 4, 5];
+
+export const obj = { name: "Aman", age: 24 };
+
+export const sum = () => {
+  console.log("addition fun...");
+};
+
+const str = "lorem ipsum";
+export default str;
+
+// array - collection of multiple values store based on index.
+// object - collection of multiple values in key-value pair
+
+// 5 words  -> sequence
+
+// types of export
+// 1. named export
+// 2. default export - limit(minimum one default export allow in each file)
---
+++ Lecture 30/script.js
@@ -0,0 +1,41 @@
+// JS - Array + DOM
+
+// import - if you want to receive/accept exported code from another file
+// export(if we have to send some block of code outside the file)
+
+// react js - multi files
+
+// folder structure
+
+// destructuring
+
+// const a = [1, 2, 3];
+// const [x, y, z] = a;
+// console.log(productOf[0]);
+// console.log(y);
+// console.log(z);
+
+// const colors = ["red", "green", "yellow", "blue", "magento"];
+// const [red, green, blue] = colors;
+
+// console.log(red);
+// console.log(green);
+// console.log(blue);
+
+// const colors = {
+//   red: "Red Color",
+//   green: "Green Color",
+//   blue: "Blue Color",
+// };
+
+// const { blue } = colors;
+// console.log(blue);
+
+// path of file while importing any exported code.
+
+import { sum, a, obj } from "./app.js";
+import x from "./app.js";
+
+console.log(x);
+console.log(sum);
+console.log(a);
